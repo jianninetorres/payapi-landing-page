999a699ffac6ebd66d7f65df1d929dc3fa7a635e
Jiannine Torres <jiannine@agilebits.com>
2020-10-28 19:51:18 -0400

    Switch header font to Libre Baskerville

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -29,7 +29,7 @@ const GlobalStyles = createGlobalStyle`
     }
 
     h1, h2, h3, h4, h5, h6 {
-        font-family: "Playfair Display", georgia, serif;
+        font-family: "Libre Baskerville", serif;
     }
 
     h1 {
